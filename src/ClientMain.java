73441e5340be892d934e63c53d4afd8a67e16b8e
FOJF <john9823@naver.com>
2020-11-17 18:03:39 +0900

    test branch

--- src/ClientMain.java
+++ src/ClientMain.java
@@ -32,6 +32,7 @@ public class ClientMain extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		//test
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
